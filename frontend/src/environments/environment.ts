55f355b31a816dfdead64f9a40af50a8b6cf6af1
zdcenter <36663840@qq.com>
2026-05-25 17:20:53 +0800

    build: configure angular dev server proxy to bypass CORS

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.149:3001/api/v1',
-  wsUrl: 'ws://192.168.1.149:3001/api/v1'
+  apiUrl: '/api/v1',
+  wsUrl: `ws://${window.location.host}/api/v1`
 };
