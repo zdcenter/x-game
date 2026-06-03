4e0616ca29c843afcf52309b0a3600bdc2d25f4b
zdcenter <36663840@qq.com>
2026-06-03 16:44:05 +0800

    update speak all room 2

--- frontend/remove_super.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = '/home/zd/x-game/frontend/src/app/features/games/math24/math24.component.ts';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace("      });\n      super();\n      effect(() => {", "      });\n      effect(() => {");
-content = content.replace("      });\n    super();\n    effect(() => {", "      });\n    effect(() => {");
-fs.writeFileSync(file, content);
